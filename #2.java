baf5fc6e665b93f6939f73004cf0556697273426
Jyoti Kumari <jk1076387@gmail.com>
2025-11-29 22:40:46 +0530

    Implement various utility functions in Solution class

---
+++ #2.java
@@ -0,0 +1,128 @@
+//Given a number n, number of apples in a bag. You and your friend are picking one apple turnwise from the bag. It is given that the first attempt is always by you. The person picking the last apple will be the winner. If you will win: print "You" (without quotes) If your friend will win: print "Friend" (without quotes)
+import java.util.*;
+
+public class Solution {
+    public static void main(String args[]) {
+        // Your code here
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%2==1){
+            System.out.println("You");
+        }
+        else{
+            System.out.println("Friend");
+        }
+    }
+}
+
+//Read a value and store it in the appropriate Java Data Type. 
+import java.util.*;
+class Solution {
+    int javaIntType(Scanner sc) {
+        // code here
+        int n=sc.nextInt();
+        return n;
+    }
+
+    String javaStringType(Scanner sc) {
+        // code here
+        sc.nextLine(); 
+        String str=sc.nextLine();
+        return str;
+    }
+    
+    float javaFloatType(Scanner sc) {
+        // code here
+        float f=sc.nextFloat();
+        return f;
+    }
+    
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        Solution obj=new Solution();
+         System.out.println(obj.javaIntType(sc));
+         System.out.println(obj.javaStringType(sc));
+         System.out.println(obj.javaFloatType(sc));
+    }
+}
+
+//Given an integer n. Write a program to return the factorial of n.Factorial of a number is the product of all the numbers from 1 till n. Note: 0 factorial is equal to 1.
+// User function Template for Java
+import java.util.*;
+class Solution {
+    public static int nFactorial(int n) {
+        int ans = 1;
+        // Write your code here
+        for(int i=1;i<=n;i++){
+            ans*=i;
+        }
+        return ans;
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        Solution obj=new Solution();
+        obj.nFactorial(n);
+    }
+}
+
+//Given two numbers a and b. The task is to find out their LCM.
+// User function Template for Java
+import java.util.*;
+class Solution {
+    public static int LCM(int a, int b) {
+
+        // write your code here
+        int lcm = Math.max(a, b);
+        while(lcm % a != 0 || lcm % b != 0){
+        lcm++;
+        }
+        // return LCM of a and b
+        return lcm;
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        Solution obj=new Solution();
+        obj.LCM(a,b);
+    }
+}
+
+//Given an integer n find the sum of the first n natural number.
+import java.util.*;
+class Solution {
+    public static int nSum(int n) {
+        // code here
+        int ans = 0;
+        for(int i=0;i<=n;i++){
+            ans+=i;
+        }
+        return ans;
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        Solution obj=new Solution();
+        obj.nSum(n);
+    }
+}
+
+
+//You are given a string s, you need to print its characters at even indices (index starts at 0).
+import java.util.*;
+class Solution {
+    public void printEvenIndices(String s) {
+        // code here
+        for(int i=0;i<s.length();i++){
+            if(i%2==0)
+               System.out.print(s.charAt(i));
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        Solution obj=new Solution();
+        obj.printEvenIndices(s);
+    }
+}
