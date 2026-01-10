ac0aa93f3707262d917f54a5b140abdaf12e1137
Jyoti Kumari <jk1076387@gmail.com>
2026-01-10 21:18:21 +0530

    Update #6.java

--- #6.java
+++ #6.java
@@ -168,3 +168,27 @@ class Solution {
     }
     
 }
+class Solution {
+    public int kthMissing(int[] arr, int k) {
+        // code here
+        int low=0;
+        int high=arr.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int missing=arr[mid]-mid+1;
+            
+            if(missing<k){
+                low=mid+1;
+            } else {
+                high=mid-1;
+            }
+        }
+      return k+low;
+    }
+    public static void main(String args[]){
+        int arr[]={2,8,9,3,7,5,6,4,1};
+        int k=3;
+        Solution obj=new Solution();
+        obj.kthMissing(arr,k);
+    }
+}
